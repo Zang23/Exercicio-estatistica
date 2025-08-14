23aedaa99fdea62266a36d95bdbdca79dc5e48b8
Zang_23 <thiagoaraujo20005@gmail.com>
2025-08-13 21:33:10 -0300

    refatorando o codigo.
    implementando tratamento dos dados "tipoMoeda" e "SimboloMoeda"
    
    Apagando colunas "alt1" e "alt2"
    
    Gerando novo arquivo CSV ao final de todo tratamento dos dados

--- dolarTratamento.py
+++ dolarTratamento.py
@@ -1,26 +1,31 @@
-import numpy as np 
+import numpy as np
 import pandas as pd #Manipulação de dados
 
 
 
-colunas = ["Datas", "codMoeda", "tipoMoeda", "simboloMoeda", "compraDolar", "vendaDolar", "alt1", "alt2"]
-
+colunas = ["data", "codMoeda", "tipoMoeda", "simboloMoeda", "compra", "venda", "alt1", "alt2"]
 
 
 dados = pd.read_csv("CotacoesMoedasPeriodo.csv", sep = ';', encoding='UTF-8',header = None, names=colunas)
 
-dados['Datas'] = dados['Datas'].astype(str).str.zfill(8)
-dados['Datas'] = pd.to_datetime(dados['Datas'], format = '%d%m%Y')
-dados['Datas'] = dados['Datas'].dt.strftime('%d/%m%/Y')
-
-numeros_float = ["compraDolar", "vendaDolar"]
+dados['data'] = dados['data'].astype(str).str.zfill(8)
+dados['data'] = pd.to_datetime(dados['data'], format = '%d%m%Y')
+dados['data'] = dados['data'].dt.strftime("%d/%m/%Y")
 
 
 
-dados["compraDolar"] = dados["compraDolar"].str.replace(',','.').astype(float) 
+colunasFloat = ["compra", "venda"]
 
+dados[colunasFloat] = dados[colunasFloat].apply(
+    lambda x: x.str.replace(",", ".").astype(float)
+)
 
+colunasString = ["tipoMoeda", "simboloMoeda"]
 
-clCompraDolar = dados["compraDolar"]
+dados[colunasString] = dados[colunasString].astype(str)
 
-#dados.to_csv(caminho do arquivo)
+colunasApagar = ["alt1", "alt2"]
+
+dados1 = dados.drop(columns=colunasApagar)
+
+dados1.to_csv("CotacoesMoedasPeriodoTratado.csv", sep=";", encoding="utf-8", index=False)
